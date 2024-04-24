b4243025bac2633174d7c5162ead52b2a32b6877
Unity Nodes <159138743+UnityNodes@users.noreply.github.com>
2024-04-24 20:07:39 +0300

    Update install-lava.sh

--- lava/install-lava.sh
+++ lava/install-lava.sh
@@ -13,7 +13,8 @@ BINARY_VERSION="v1.2.0"
 
 source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/dependencies.sh)
 
-printLine
+### Bulding binaries
+echo ""
 printColor blue "4. Building binaries"
 
 export LAVA_BINARY=lavad
@@ -31,12 +32,80 @@ lavad config keyring-backend test
 lavad config chain-id $CHAIN_ID
 lavad init "$NODE_MONIKER" --chain-id $CHAIN_ID
 
-printLine
-printColor blue "5.Download genesis and addrbook"
+### Downoload genesis and addrbook
+echo ""
+printColor blue "5. Download genesis and addrbook"
+
 curl -s https://raw.githubusercontent.com/lavanet/lava-config/main/testnet-2/genesis_json/genesis.json > $HOME/.lava/config/genesis.json
 curl -Ls https://snapshots.aknodes.net/snapshots/lava/addrbook.json > $HOME/.lava/config/addrbook.json
 
-printLine
-printColor blue "Downloading snapshot for fast synchronization" 
+### Seed and peers
+echo ""
+printColor "Configuring seed and peers"
+
+SEEDS="3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@testnet2-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@testnet2-seed-node2.lavanet.xyz:26656"
+PEERS=""
+sed -i 's|^seeds *=.*|seeds = "'$SEEDS'"|; s|^persistent_peers *=.*|persistent_peers = "'$PEERS'"|' $HOME/.lava/config/config.toml
+
+sed -i 's|^pruning *=.*|pruning = "custom"|g' $HOME/.lava/config/app.toml
+sed -i 's|^pruning-keep-recent  *=.*|pruning-keep-recent = "100"|g' $HOME/.lava/config/app.toml
+sed -i 's|^pruning-interval *=.*|pruning-interval = "10"|g' $HOME/.lava/config/app.toml
+sed -i 's|^snapshot-interval *=.*|snapshot-interval = 0|g' $HOME/.lava/config/app.toml
+
+sed -i 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.025ulava"|g' $HOME/.lava/config/app.toml
+sed -i 's|^prometheus *=.*|prometheus = true|' $HOME/.lava/config/config.toml
+
+sed -i \
+  -e 's/timeout_commit = ".*"/timeout_commit = "30s"/g' \
+  -e 's/timeout_propose = ".*"/timeout_propose = "1s"/g' \
+  -e 's/timeout_precommit = ".*"/timeout_precommit = "1s"/g' \
+  -e 's/timeout_precommit_delta = ".*"/timeout_precommit_delta = "500ms"/g' \
+  -e 's/timeout_prevote = ".*"/timeout_prevote = "1s"/g' \
+  -e 's/timeout_prevote_delta = ".*"/timeout_prevote_delta = "500ms"/g' \
+  -e 's/timeout_propose_delta = ".*"/timeout_propose_delta = "500ms"/g' \
+  -e 's/skip_timeout_commit = ".*"/skip_timeout_commit = false/g' \
+  -e 's/seeds = ".*"/seeds = "3a445bfdbe2d0c8ee82461633aa3af31bc2b4dc0@testnet2-seed-node.lavanet.xyz:26656,e593c7a9ca61f5616119d6beb5bd8ef5dd28d62d@testnet2-seed-node2.lavanet.xyz:26656"/g' \
+  $HOME/.lava/config/config.toml
+
+sed -i -e 's/broadcast-mode = ".*"/broadcast-mode = "sync"/g' $HOME/.lava/config/config.toml
+
+### Create service
+echo ""
+printColor blue "Create  service"
+
+sudo tee /etc/systemd/system/lavad.service > /dev/null << EOF
+[Unit]
+Description=Lava Network Node
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which lavad) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=10000
+[Install]
+WantedBy=multi-user.target
+EOF
+
+### Downoload snapshot
+echo ""
+printColor blue "6. Downloading snapshot for fast synchronization" 
+
 lavad tendermint unsafe-reset-all --home $HOME/.lava --keep-addr-book 
 curl https://snapshots.aknodes.net/snapshots/lava/snapshot-lava.AKNodes.lz4 | lz4 -dc - | tar -xf - -C $HOME/.lava
+
+### Start service and run node
+echo ""
+printColor blue "Start service and run node"
+
+sudo systemctl daemon-reload
+sudo systemctl enable lavad
+sudo systemctl start lavad
+
+### Useful commands
+echo ""
+printLine
+printColor yellow "Check logs lava node >>> journalctl -u lavad -f --no-hostname -o cat "
+printColor yellow "Check synchonization >>> lavad status | jq | grep "catching_up" "
+printColor yellow "Enjoy to Unity Nodes >>>  https://t.me/unitynodes "
+printLine
