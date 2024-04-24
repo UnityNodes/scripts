c53425e8b5c85b5f71cdfca391e2ac830690add1
Unity Nodes <159138743+UnityNodes@users.noreply.github.com>
2024-04-24 20:25:47 +0300

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -3,15 +3,15 @@
 source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/utils.sh)
 
 echo ""
-printColor blue "1/6 Updating packages"
+printColor blue "[1/6] Updating packages"
 sudo apt update && sudo apt upgrade
 
 echo ""
-printColor blue "2/6 Install dependencies"
+printColor blue "[2/6] Install dependencies"
 sudo apt install -y lz4 jq make git gcc build-essential curl chrony unzip gzip snapd tmux bc
 
 echo ""
-printColor blue "3/6 Install go" && sleep 1
+printColor blue "[3/6] Install go" && sleep 1
 if ! [ -x "$(command -v go)" ]; then
   source <(curl -s "https://raw.githubusercontent.com/UnityNodes/scripts/main/install-go.sh")
   source .bash_profile
