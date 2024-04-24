4e573ddb5b4a705ac788ab45e1c45d82cd58e910
Unity Nodes <159138743+UnityNodes@users.noreply.github.com>
2024-04-24 19:17:29 +0300

    Update install-lava.sh

--- lava/install-lava.sh
+++ lava/install-lava.sh
@@ -12,9 +12,11 @@ CHAIN_ID="lava-testnet-2"
 BINARY_VERSION="v1.2.0"
 
 source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/dependencies.sh)
-
+printLine
 printColor blue "### Building binaries ###"
+
 export LAVA_BINARY=lavad
+
 cd || return
 rm -rf lava
 git clone https://github.com/lavanet/lava
