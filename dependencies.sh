78052dc3c0857604eced46bc268f2ee86f1c08a6
Fearless Oleksii <143862878+CryptoManUA@users.noreply.github.com>
2024-04-25 21:19:09 +0300

    Update dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -3,21 +3,27 @@
 source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/utils.sh)
 
 echo ""
+anim
+echo ""
 printColor blue "[1/6] Updating packages"
 sudo apt update && sudo apt upgrade
-anim
 
+
+echo ""
+anim
 echo ""
 printColor blue "[2/6] Install dependencies"
 sudo apt install -y lz4 jq make git gcc build-essential curl chrony unzip gzip snapd tmux bc
-anim
 
+
+echo ""
+anim
 echo ""
 printColor blue "[3/6] Install go" && sleep 1
 if ! [ -x "$(command -v go)" ]; then
   source <(curl -s "https://raw.githubusercontent.com/UnityNodes/scripts/main/install-go.sh")
   source .bash_profile
-  anim
+  
 fi
 
 echo "$(go version)"
