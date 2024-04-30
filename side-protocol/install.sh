ff19e0bd56e698342e00975960614895395eccc7
Unity Nodes <159138743+UnityNodes@users.noreply.github.com>
2024-04-30 19:27:19 +0300

    Update install.sh

--- side-protocol/install.sh
+++ side-protocol/install.sh
@@ -1,1 +1,88 @@
 
+#!/bin/bash
+
+### Utils Unity Nodes
+source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/utils.sh)
+
+clear
+logo
+
+echo -e "\e[30;47m Please enter the node moniker:\e[0m"
+echo -en ">>> "
+read -r NODE_MONIKER
+
+### Install Dependencies
+source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/dependencies.sh)
+
+### Building binaries
+echo ""
+printColor blue "[4/6] Building binaries"
+
+cd $HOME
+rm -rf sidechain
+git clone https://github.com/sideprotocol/sidechain.git
+cd sidechain
+git checkout v0.7.0-rc2
+make install
+
+sided config chain-id side-testnet-3
+sided config keyring-backend test
+sided config node tcp://localhost:26657
+source $HOME/.bash_profile
+
+sided init "$NODE_MONIKER" --chain-id side-testnet-3
+
+### Download genesis and addrbook
+curl -L https://snapshots-testnet.nodejumper.io/side-testnet/genesis.json > $HOME/.side/config/genesis.json
+curl -L https://snapshots-testnet.nodejumper.io/side-testnet/addrbook.json > $HOME/.side/config/addrbook.json
+
+### Seed config
+sed -i -e 's|^seeds *=.*|seeds = "6decdc5565bf5232cdf5597a7784bfe828c32277@158.220.126.137:11656,e9ee4fb923d5aab89207df36ce660ff1b882fc72@136.243.33.177:21656,9c14080752bdfa33f4624f83cd155e2d3976e303@side-testnet-seed.itrocket.net:45656"|' $HOME/.side/config/config.toml
+
+### Minimum gas price
+sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.005uside"|' $HOME/.side/config/app.toml
+
+### Set pruning
+sed -i \
+  -e 's|^pruning *=.*|pruning = "custom"|' \
+  -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
+  $HOME/.side/config/app.toml
+
+### Create service
+sudo tee /etc/systemd/system/sided.service > /dev/null << EOF
+[Unit]
+Description=Side node service
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=$(which sided) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+### Download snapshot
+echo ""
+printColor blue "[5/6] Downloading snapshot for fast synchronization" 
+curl "https://snapshots-testnet.nodejumper.io/side-testnet/side-testnet_latest.tar.lz4" | lz4 -dc - | tar -xf - -C "$HOME/.side"
+
+### Start service and run node
+echo ""
+printColor blue "[6/6] Start service and run node"
+
+sudo systemctl daemon-reload
+sudo systemctl enable sided.service
+sudo systemctl start sided.service
+
+### Useful commands
+echo ""
+printLine
+printColor blue "Check your logs        >>> journalctl -u sided -f --no-hostname -o cat "
+echo ""
+printColor blue "Check synchronization  >>> sided status | jq | grep \"catching_up\" "
+echo ""
+printColor blue "Enjoy Unity Nodes      >>> https://t.me/unitynodes "
+printLine
