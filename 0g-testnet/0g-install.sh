8c4c1c1f52cf09b4db50bf5bddeb5252158a1bd8
Unity Nodes <159138743+UnityNodes@users.noreply.github.com>
2024-05-28 14:37:09 +0300

    Update 0g-install.sh

--- 0g-testnet/0g-install.sh
+++ 0g-testnet/0g-install.sh
@@ -52,6 +52,9 @@ sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_rec
 sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.0gchain/config/app.toml
 sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.0gchain/config/app.toml
 
+# Download snapshot
+curl "https://snapshots-testnet.nodejumper.io/0g-testnet/0g-testnet_latest.tar.lz4" | lz4 -dc - | tar -xf - -C "$HOME/.0gchain"
+
 ### Create service
 sudo tee /etc/systemd/system/0gchaind.service > /dev/null << EOF
 [Unit]
