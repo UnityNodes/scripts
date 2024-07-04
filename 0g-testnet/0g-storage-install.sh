83917bf0e38064609d5368be5ad65499911e51dd
Fearless Oleksii <143862878+0xFearless-1@users.noreply.github.com>
2024-07-04 09:43:23 +0300

    Update 0g-storage-install.sh

--- 0g-testnet/0g-storage-install.sh
+++ 0g-testnet/0g-storage-install.sh
@@ -54,9 +54,9 @@ cd $HOME
 
 0gchaind keys unsafe-export-eth-key wallet
 
-printColor yellow "if the node and wallet is created on this server you should see your private key"
-printColor red "Copy it for further installation"
-printColor yellow "if the node and wallet is created on another server, just use it in the subsequent installation"
+printColor blue "if the node and wallet is created on this server you should see your private key"
+printColor blue "Copy it for further installation"
+printColor blue "if the node and wallet is created on another server, just use it in the subsequent installation"
 read -p "Continue installation? (y/n): " choice
 case "$choice" in
   y|Y ) 
