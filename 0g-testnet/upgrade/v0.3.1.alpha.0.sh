5f41da9057fd8ac7d1757920ca61b0ac03c669cc
Asapov <137000528+asapov01@users.noreply.github.com>
2024-08-11 00:23:41 +0300

    Update v0.3.1.alpha.0.sh

--- 0g-testnet/upgrade/v0.3.1.alpha.0.sh
+++ 0g-testnet/upgrade/v0.3.1.alpha.0.sh
@@ -1,1 +1,8 @@
+sudo systemctl stop 0gchaind
 
+cd $HOME
+rm -rf 0g-chain
+wget -O 0gchaind https://github.com/0glabs/0g-chain/releases/download/v0.3.1.alpha.0/0gchaind-linux-v0.3.1.alpha.0
+chmod +x $HOME/0gchaind
+sudo mv $HOME/0gchaind $(which 0gchaind)
+sudo systemctl restart 0gchaind
