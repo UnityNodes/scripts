803a6b616069ae94ab5377bd434cd8ce06f7764c
Asapov <137000528+asapov01@users.noreply.github.com>
2024-08-11 22:20:58 +0300

    Update 0g-install.sh

--- 0g-testnet/0g-install.sh
+++ 0g-testnet/0g-install.sh
@@ -17,10 +17,13 @@ source <(curl -s https://raw.githubusercontent.com/UnityNodes/scripts/main/depen
 echo ""
 printColor blue "[4/6] Building binaries"
 
-cd $HOME
-rm -rf 0g-chain
-git clone -b v0.2.5 https://github.com/0glabs/0g-chain.git
+# Clone project repository
+cd && rm -rf 0g-chain
+git clone https://github.com/0glabs/0g-chain
 cd 0g-chain
+git checkout v0.3.1.alpha.1
+
+# Build binary
 make install
 
 0gchaind config chain-id zgtendermint_16600-2
