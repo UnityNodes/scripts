80ac2ac7e82b43d611e171c198cce55a07120196
Asapov <137000528+asapov01@users.noreply.github.com>
2024-09-02 12:13:46 +0300

    Create v0.4.2.sh

---
+++ warden/upgrade/v0.4.2.sh
@@ -0,0 +1,10 @@
+cd $HOME
+rm -rf download
+mkdir download
+cd download
+wget https://github.com/warden-protocol/wardenprotocol/releases/download/v0.4.2/wardend_Linux_x86_64.zip
+unzip wardend_Linux_x86_64.zip
+rm wardend_Linux_x86_64.zip
+chmod +x $HOME/download/wardend
+sudo mv $HOME/download/wardend $(which wardend)
+sudo systemctl restart wardend
