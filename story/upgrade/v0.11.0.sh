83895dac2ec16a05f4d1e0e65ec61f432ee156ba
Asapov <137000528+asapov01@users.noreply.github.com>
2024-10-09 00:23:35 +0300

    Update v0.11.0.sh

--- story/upgrade/v0.11.0.sh
+++ story/upgrade/v0.11.0.sh
@@ -1,1 +1,9 @@
-.
+cd $HOME
+rm -rf story
+git clone https://github.com/piplabs/story
+cd $HOME/story
+git checkout v0.11.0
+go build -o story ./client
+sudo mv $HOME/story/story $(which story)
+sudo systemctl restart story
+sudo journalctl -u story -f -o cat
