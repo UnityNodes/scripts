3f40345723c6fbcf1330c45f4830f696541c102b
Asapov <137000528+asapov01@users.noreply.github.com>
2024-10-15 23:03:14 +0300

    Create v0.4.0.sh

---
+++ 0g-testnet/upgrade/v0.4.0.sh
@@ -0,0 +1,8 @@
+sudo systemctl stop 0gchaind
+
+cd $HOME
+rm -rf 0g-chain
+wget -O 0gchaind https://github.com/0glabs/0g-chain/releases/download/v0.4.0/0gchaind-linux-v0.4.0
+chmod +x $HOME/0gchaind
+sudo mv $HOME/0gchaind $(which 0gchaind)
+sudo systemctl restart 0gchaind
