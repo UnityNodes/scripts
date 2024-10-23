cdbde385db4a6ef6c15f9a8cd9ea7516a7b9377e
Asapov <137000528+asapov01@users.noreply.github.com>
2024-10-23 16:03:05 +0300

    Update install-warden.sh

--- warden/install-warden.sh
+++ warden/install-warden.sh
@@ -21,36 +21,35 @@ cd $HOME
 rm -rf wardenprotocol
 git clone https://github.com/warden-protocol/wardenprotocol
 cd wardenprotocol
-git checkout v0.4.2
+git checkout v0.5.2
 make install
 
-wardend config set client chain-id buenavista-1
+wardend config set client chain-id chiado_10010-1
 wardend config set client keyring-backend test
 wardend config set client node tcp://localhost:26657
 source $HOME/.bash_profile
 
-wardend init "$NODE_MONIKER" --chain-id buenavista-1
+wardend init "$NODE_MONIKER" --chain-id chiado_10010-1
 
-### Download addrbook
-curl -L https://buenavista-genesis.s3.eu-west-1.amazonaws.com/genesis.json.tar.xz | tar xJf -
-mv genesis.json $HOME/.warden/config/genesis.json
+### Download addrbook, genesis.json
+curl -L https://snapshots-testnet.unitynodes.com/warden-testnet/genesis.json > $HOME/.warden/config/genesis.json
 curl -L https://snapshots-testnet.unitynodes.com/warden-testnet/addrbook.json > $HOME/.warden/config/addrbook.json
 
 ### Set seeds
-sed -i -e 's|^seeds *=.*|seeds = "ddb4d92ab6eba8363bab2f3a0d7fa7a970ae437f@sentry-1.buenavista.wardenprotocol.org:26656,c717995fd56dcf0056ed835e489788af4ffd8fe8@sentry-2.buenavista.wardenprotocol.org:26656,e1c61de5d437f35a715ac94b88ec62c482edc166@sentry-3.buenavista.wardenprotocol.org:26656"|' $HOME/.warden/config/config.toml
+sed -i -e 's|^seeds *=.*|seeds = "2d2c7af1c2d28408f437aef3d034087f40b85401@52.51.132.79:26656,8288657cb2ba075f600911685670517d18f54f3b@warden-testnet-seed.itrocket.net:18656"|' $HOME/.warden/config/config.toml
 
-### Set peers
-PEERS="92ba004ac4bcd5afbd46bc494ec906579d1f5c1d@52.30.124.80:26656,ed5781ea586d802b580fdc3515d75026262f4b9d@54.171.21.98:26656"
+# Set peers
+PEERS="8288657cb2ba075f600911685670517d18f54f3b@65.108.231.124:18656,5461e7642520a1f8427ffaa57f9d39cf345fcd47@54.72.190.0:26656,22e706be097baf72b999a6b00b5e1bba5540675c@135.181.81.254:18656"
 sed -i "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.warden/config/config.toml
 
-### Minimum gas price
-sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "0.01uward"|' $HOME/.warden/config/app.toml
+# Set minimum gas price
+sed -i -e 's|^minimum-gas-prices *=.*|minimum-gas-prices = "25000000award"|' $HOME/.warden/config/app.toml
 
 ### Set pruning
 sed -i \
   -e 's|^pruning *=.*|pruning = "custom"|' \
   -e 's|^pruning-keep-recent *=.*|pruning-keep-recent = "100"|' \
-  -e 's|^pruning-interval *=.*|pruning-interval = "17"|' \
+  -e 's|^pruning-interval *=.*|pruning-interval = "10"|' \
   $HOME/.warden/config/app.toml
 
 ### Create service
@@ -84,7 +83,7 @@ sudo systemctl start wardend.service
 ### Useful commands
 echo ""
 printLine
-printColor blue "Check your logs        >>> journalctl -u wardend -f --no-hostname -o cat "
+printColor blue "Check your logs        >>> journalctl -u wardend -f -o cat "
 echo ""
 printColor blue "Check synchronization  >>> wardend status | jq | grep \"catching_up\" "
 echo ""
